58f898bca222916ac42d46c5a991949c3775100a
aleks-luk <a.lukaszewsky@gmail.com>
2024-11-08 16:34:42 +0100

    separate setup and migration scripts

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,20 +2,12 @@ export MYSQL_PASSWORD="MySqlPasswordHere"
 export MYSQL_ROOT_PASSWORD="MySqlRootPasswordHere"
 export POSTGRES_PASSWORD="PostgresPasswordHere"
 
-echo "Creating virtual environment..."
 python3 -m venv venv
 
-echo "Activating virtual environment..."
 source venv/bin/activate
 
-echo "Installing requirements..."
 pip install -r requirements.txt
 
-echo "Setting up Docker containers..."
+docker-compose pull
+
 docker-compose up -d
-
-echo "Generating sample data..."
-python generate_data.py
-
-echo "Running data migration..."
-python migrate.py
