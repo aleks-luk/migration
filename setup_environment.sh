1529a692f58aaf54a540bab59fd6f25733bb1652
aleks-luk <a.lukaszewsky@gmail.com>
2024-11-08 18:11:40 +0100

    change the json file and documentation

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,3 @@
-export MYSQL_PASSWORD="MySqlPasswordHere"
-export MYSQL_ROOT_PASSWORD="MySqlRootPasswordHere"
-export POSTGRES_PASSWORD="PostgresPasswordHere"
-
 python3 -m venv venv
 
 source venv/bin/activate
